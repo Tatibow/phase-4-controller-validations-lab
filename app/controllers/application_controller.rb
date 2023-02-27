686b699ba4a4b5d23b4a350c4c58cac131d315d8
Tatiana <Bowmantatiana2@gmail.com>
2023-02-27 06:46:09 -0800

    commit tree 14ebbae117e52381529320260642d297ec002a0c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+# rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+# rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+# private
+
+# def render_invalid_record(invalid)
+#     render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+# end
+
+#   def render_not_found
+#     render json: {error: "not found"}, status: :not_found
+#   end
+
 end
